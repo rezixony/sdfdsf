52b899ff3d7813802e8136ca6d0215251d8838cd
Valeria Shepel <vashep@ttu.ee>
2022-03-16 14:20:29 +0200

    Add project files.

---
+++ sdfdsf/Form1.vb
@@ -0,0 +1,15 @@
+﻿Imports HtmlAgilityPack
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim url = "https://www.worldometers.info/coronavirus/"
+        Dim web As HtmlWeb = New HtmlWeb()
+        Dim htmlDoc = web.Load(url)
+        Dim node = htmlDoc.DocumentNode.SelectSingleNode("//*[translate(@id,'ABCDEFGHIJKLMNOPQRSTUVWXYZ','abcdefghijklmnopqrstuvwxyz')='maincounter-wrap']/div/span")
+        MsgBox(node.InnerHtml.ToString)
+        txtCoronaCases.Text = node.InnerHtml.ToString
+        'MsgBox(htmlDoc.DocumentNode.InnerHtml)
+    End Sub
+
+
+End Class
